63773476442f537e33e0a01f001d29f88a13b9d1
jacobgreen1984 <jacobgreen4477.gmail.com>
2018-06-26 18:07:41 +0900

    a

--- kaggle/titanic/fe_impute.R
+++
@@ -1,9 +0,0 @@
-
-
-fe_impute <- function(data){
-  for(i in names(which(sapply(data, is.numeric)))){
-    miss_index <- which(is.na(data[,i]))
-    data[miss_index, i] <- median(data[, i], na.rm=TRUE)
-  }
-  return(data)
-}
